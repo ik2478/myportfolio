6ddf4a82abcaf27fcc43e707100e2d0b99c25915
ik2478 <ik247@yahoo.com>
2018-05-08 17:51:01 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+
+// var pad =document.querySelector("pad");
+// pad.addEventListener("mouseover", function padAdd(){
+// 	//pad.classList.toggle("animated fadeIn")
+// 	pad.innerHTML = txt;
+
+// })
+
+// $(document).ready(function(){
+// 	$("button").click(function(){
+// 		$("button").hide();
+// 	})
+// })
+
+
+// $(document).ready(function(){
+//     $(".pad").hover(function(){
+//         $("p").append(" <b>Appended text</b>.");;
+//     },
+//     function(){
+//         alert("Bye! You now leave p1!");
+//     }); 
+// });
+
+// $(document).ready(function(){
+// 	$(".pad").click(function(){
+//  		var text = `<a href="https://face-identification.herokuapp.com/"><img src="https://res.cloudinary.com/ik247/image/upload/c_scale,w_662/v1524953078/Screen_Shot_2018-04-28_at_4.47.42_PM.png"   /></a>`
+//  		//$("pad").hide();
+//  		$(".pad").append(text);
+//  	})
+//  })
+
+// $(document).ready(function(){
+//     $("body").niceScroll({
+        
+//         cursorwidth:6,
+// 		cursoropacitymin:0.4,
+// 		cursorcolor:'grey',
+		
+// 		cursorborder:'2px solid darkgrey',
+// 		cursorborderradius:4,
+// 		autohidemode:'leave'
+// 		    });
+
+//     $(function() {
+//         $('a.page-scroll').bind('click', function(event) {
+//             var $anchor = $(this);
+//             $('html, body').stop().animate({
+//                 scrollTop: $($anchor.attr('href')).offset().top
+//             }, 1000);
+//             event.preventDefault();
+//         });
+//     });
+
+//     var headerHeight = $(".s").outerHeight(true);
+//     $(" #about, #home, #portfolio").css("padding-top", headerHeight )
+//     $(" #about, #portfolio").css("padding-bottom", headerHeight )
+// });
